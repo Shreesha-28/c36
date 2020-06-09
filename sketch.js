2cda6d22ddd4b99066e2a89d92438b9e8d7b4ca7
Shreesha-28 <63409740+Shreesha-28@users.noreply.github.com>
2020-06-09 20:06:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var gameState=0;
+var playerCount,database;
+var form,game,player;
+var allPlayers;
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+    console.log(database);
+    game=new Game();
+    game.getState();
+    game.start();
+
+
+}
+
+function draw(){
+    background("white");
+
+    if(playerCount===4){
+        game.update(1);
+    }
+    if(gameState===1){
+     clear();
+        game.play();
+    }
+    
+}
+
